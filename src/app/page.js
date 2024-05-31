94d533034a34c0b0946a370b4fffc0fa7837a185
Anish202020 <anishkumarbdmi@gmail.com>
2024-05-31 20:22:36 +0530

    commit changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
+import Results from '@/components/Results';
 import React from 'react'
-// const API_KEY = process.env.API_KEY;
-const API_KEY = "b35d45d019144faa9bcfcb91733f6aad";
+const API_KEY = process.env.API_KEY;
+
 
 export default async function Home({ searchParams }) {
   
   const genre = searchParams.genre || 'fetchTrending';
+
+
   const res = await fetch(
     `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
@@ -20,7 +23,7 @@ export default async function Home({ searchParams }) {
 
   return (
     <div>
-      Home
+      <Results results={results}/>
     </div>
   );
 }
